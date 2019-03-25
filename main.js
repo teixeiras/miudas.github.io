9e6064a62f22d1c12c235d6917d4e41e64b2614c
fteixeira <763604+teixeiras@users.noreply.github.com>
2019-03-25 16:12:23 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-import { main } from 'src/main'; // or './module'
-let val = main();  // val is "Hello";
+import * as myModule from 'src/main.js';
+let val = muModule.main();  // val is "Hello";
 console.log(val);
